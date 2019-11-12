0dcf2f23cb0a13589db040fd08984a6a845664d5
lsussman1 <48657545+lsussman1@users.noreply.github.com>
2019-11-12 12:38:46 +1100

    Add files via upload

--- client.py
+++ client.py
@@ -1,38 +1,53 @@
 from socket import *
+import select
 import sys
 
 #Server would be running on the same host as Client
 IP = sys.argv[1]
 port = int(sys.argv[2])
-buffer = 1024
+buffer = 4096
+UPDATE_INTERVAL = 0.5
 
 def Main(): 
-    sock = socket(AF_INET, SOCK_STREAM)
-    # connect to server on local computer 
-    sock.connect((IP,port)) 
-    # message you send to server 
-    while True: 
-        data = sock.recv(buffer) 
-        if data:
-            recieveHandler(data, sock)
-        # print the received message 
-        # here it would be a reverse of sent message 
-        #print('Received from the server :',str(data.decode('ascii'))) 
-  
+    s = socket(AF_INET, SOCK_STREAM)
+    s.connect((IP, port)) 
+    s.settimeout(UPDATE_INTERVAL)
+    message = ""
+    while message != 'logout': 
+        sys.stdout.write('>')
+        if message != "":           
+            sendHandler(message, s)
+        try:
+            data = s.recv(buffer)
+            while data:
+                recieveHandler(data, s)
+                data = None
+        except timeout:
+            sys.stdout.write('>')
+            message = sys.stdin.readline()
 
-    # close the connection 
-    socket.close() 
-def recieveHandler(data, sock):
-    decoded = data.decode('utf-8')
+    s.close() 
+
+#doesn't succesfully remove header for everything
+def recieveHandler(encoded, s):
+    decoded = encoded.decode('utf-8')
+    print(decoded)
     split = decoded.split(' ', 1)
     head = split[0]
     message = split[1]
     if head == "question":
         response = input(message + " ")
         encoded = response.encode('utf-8')
-        sock.sendto(encoded, (IP, port))
-    if head == "statement":
+        s.sendto(encoded, (IP, port))
+    elif head == "statement":
         print(message)
+    else:
+        print("not expected header")
 
+def sendHandler(decoded, s):
+    encoded = decoded.encode('utf-8')
+    s.sendto(encoded, (IP, port))
+    
+    
 if __name__ == '__main__': 
     Main() 
